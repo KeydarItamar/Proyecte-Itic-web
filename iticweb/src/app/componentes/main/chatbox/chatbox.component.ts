a83333cb5f772506df417d8fadd023a01565964e
“Itamar <Ikeydar@iris-eng.com>
2024-02-23 15:27:43 +0100

    creacion Db + chat component backend

--- iticweb/src/app/componentes/main/chatbox/chatbox.component.ts
+++ iticweb/src/app/componentes/main/chatbox/chatbox.component.ts
@@ -3,26 +3,38 @@ import { NgForm } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { NgModel } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
-
+import { ChatService } from 'src/app/services/chat.service';
 @Component({
   selector: 'app-chatbox',
   templateUrl: './chatbox.component.html',
   styleUrls: ['./chatbox.component.css']
 })
 export class ChatboxComponent implements OnInit {
-  query= '';
+  
   listaQuerys: string[]= []
   respuestas: string[]=[]
-  constructor() { }
+  resultado: string=''
+
+  constructor(private chatService: ChatService) { }
 
   ngOnInit(): void {
   }
+ 
 
-
-  iaChat(valor: string){
-    this.listaQuerys.push(valor);
-    this.respuestas.push('yes')
-    
+  
+  iaChat(query: string): void {
+    this.chatService.enviarDatosAlBackend(query)
+      .subscribe({
+         next: response => {
+          this.resultado = response;
+          this.respuestas.push(this.resultado)
+        },
+        error: error => {
+          console.error('Error al llamar al backend:', error);
+        }
+       });
+    query = '';
   }
+
 
 }
--- iticweb/src/app/services/chat.service.ts
+++ iticweb/src/app/services/chat.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
-  constructor() { }
+constructor(private http: HttpClient) { }
+
+apiUrl= 'http://localhost:3000'
+
+  enviarDatosAlBackend(query: string): Observable<any> {
+    return this.http.post<string>(
+      `${this.apiUrl}/enviar-datos`,
+      { query },
+      { responseType: 'text' as 'json' } 
+    );
+  
+  }
+
 }
