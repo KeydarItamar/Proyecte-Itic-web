d7437007976d687bdee8f3e8cefc1dfdecdefa13
Oscar Pérez <2023_oscar.perez@iticbcn.cat>
2024-04-15 16:13:47 +0200

    eliminar noticia desde gestor hecho

--- iticweb/src/app/componentes/noticias/gestor-noticias/gestor-noticias.component.ts
+++ iticweb/src/app/componentes/noticias/gestor-noticias/gestor-noticias.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NoticiasService } from 'src/app/noticias.service';
 import { Noticia } from '../noticia-detalle/noticia';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-gestor-noticias',
@@ -8,10 +9,9 @@ import { Noticia } from '../noticia-detalle/noticia';
   styleUrls: ['./gestor-noticias.component.css']
 })
 export class GestorNoticiasComponent implements OnInit {
-
   noticias: number[] = [] 
 
-  constructor( private noticia : NoticiasService) { }
+  constructor( private noticia : NoticiasService, private router: Router) { }
   noticiaDetalle = "noticiaDetalle"
   allNoticias=[]
   miniNoticia!: Noticia;
@@ -27,7 +27,38 @@ export class GestorNoticiasComponent implements OnInit {
   }
   currentIndex = 1;
 
-  selectAllNoticias()  {
+  // Método para navegar al formulario de edición con los datos de la noticia pre-cargados
+  editarNoticia(noticia: any) {
+    const id = noticia["id"];
+    this.noticia.getNoticia(id).subscribe({
+      next: response => {
+        console.log(response)
+        const datosNoticia = response; // Datos de la noticia obtenidos del backend
+        this.router.navigate(['/insert_noticia/', { id }]);
+      },
+      error: error => {
+        console.log('Error al obtener los detalles de la noticia: ' + error);
+      }
+    });
+  }
+  
+  // Método para eliminar una noticia
+  deleteNoticia(noticia: any) {
+    if (confirm("Segur que vols esborrar la notícia?")) {
+      const id = noticia["id"];
+      this.noticia.deleteNoticia(id).subscribe({
+        next: response => {
+          console.log("Noticia borrada exitosamente.");
+        },
+        error: error => {
+          console.error("Error al borrar la noticia:", error);
+        }
+      });
+      window.location.reload();
+    }
+  }
+
+  selectAllNoticias() {
     this.noticia.getAllNoticias().subscribe({
       next: response =>{
         this.allNoticias = response
