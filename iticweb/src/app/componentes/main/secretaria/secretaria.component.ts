82175619b3c192db78ceb0803729268fd6d5062b
Oscar Pérez <2023_oscar.perez@iticbcn.cat>
2024-04-23 16:16:20 +0200

    agregada funcionalidad del editor en secretaria, falta que los datos sean dinamicos

--- iticweb/src/app/componentes/main/secretaria/secretaria.component.ts
+++ iticweb/src/app/componentes/main/secretaria/secretaria.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 declare var tinymce: any;
@@ -9,15 +9,31 @@ declare var tinymce: any;
   styleUrls: ['./secretaria.component.css']
 })
 export class SecretariaComponent implements OnInit {
+  editorVisible: boolean = false;
+  initialContent: string = ''; // Guarda el contenido inicial del editor
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  mostrarEditor() {
+    this.editorVisible = true;
+    document.body.style.overflow = 'hidden'; // Evitar el desplazamiento de la página
+  }
+
+  cerrarEditor() {
+    this.editorVisible = false;
+    document.body.style.overflow = ''; // Restaurar el desplazamiento de la página
+  }
+
+  cancelarEdicion() {
+    this.cerrarEditor();
+  }
+
   editarContent(entryContent: HTMLElement) {
     const editorContent = tinymce.get('editor').getContent();
-
     entryContent.innerHTML = editorContent;
+    this.cerrarEditor();
   }
 }
