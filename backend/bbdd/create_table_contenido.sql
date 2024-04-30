beb583e6290ef2434c1f2fefb0591ccfa2840ed4
“Itamar <Ikeydar@iris-eng.com>
2024-04-30 16:00:29 +0200

    Cambios en componentes editables

--- backend/bbdd/create_table_contenido.sql
+++ backend/bbdd/create_table_contenido.sql
@@ -25,3 +25,55 @@ CREATE TABLE IF NOT EXISTS Articulos (
         --             de baixa del centre</a></li>
         -- </ul>')
 
+-- *****************************************************+
+--  <div class="texto"> 
+--         <div class="conteiner-titulo">
+--             <h1 class="titulo">On som</h1>
+--         </div>
+        
+--         <br>
+--         <p>C/ Sancho de Avila, 131</p>
+--         <p>08013 Barcelona</p>
+--         <br>
+--         <p><strong>Contacte:</strong> iticbcn(a)xtec.cat</p>
+--         <br>
+--         <div class="separador"></div>
+--         <br>
+--     <h3>Com arribar-hi</h3>
+--         <br>
+--     <ul>
+--         <li>Per tren:<strong> Rodalies R1</strong> + V25</li>
+--         <li>Per bus:<strong> V25</strong></li>
+--         <li>Per metro:<strong> L1</strong> (Glòries) / <strong>L4</strong>  (Llacuna)</li>
+--         <li>Tranvía:<strong> T4</strong> (Ca l’Aranyò)</li>
+--     </ul>
+--     </div>
+--     <div class="div-img"><img class="mapa" src="assets/imagenes/mapa.png" alt="mapa Itic"></div>
+
+
+-- **************++++*******************************************************
+-- Organigrama
+
+-- <main>
+--     <h1 class="titulo">Organigrama</h1>
+--     <div class="contenido">
+--         <div class="texto">
+--         <p>Els òrgans col·legiats del nostre centre són:</p>
+--         <br>
+--         <ul>
+--             <li>El <a>Consell escolar </a>del centre</li>
+--             <li>El Claustre del professorat, compost per tot el professorat assignat a l’Institut.</li>
+--         </ul>
+       
+--         <br>    
+--         <p>Les dependències funcionals, del personal que formem part de l’Institut TIC de Barcelona, es mostren en el
+--             següent esquema de dependències (organigrama funcional), on la part central correspon als membres de l’equip directiu:
+--         </p>
+--         </div>
+--         <div class="img-container">
+--         <img src="/assets/organigrama/ITICBCN-Organigrama-funcional.png" alt="">
+--         <br>
+--         <img src="/assets/organigrama/ITICBCN-Equip-directiu.png" alt="">
+--         </div>
+--     </div>
+-- </main>
